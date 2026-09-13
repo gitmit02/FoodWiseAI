bee560c016cb8b4cc80d62550ab355f083400717
Mitali <mitalirautela02@gmail.com>
2026-09-13 20:31:20 +0530

    Change API base URL to production server

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://foodwiseai.onrender.com/";
 
 export async function analyzeFoodWaste(payload) {
   const res = await axios.post(`${API_BASE_URL}/api/analyze`, payload);
